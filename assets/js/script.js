2ac71ad0666d5b46e5bbe1fe88af9a4ba8ac5f56
Nilesh Vishwakarma <nileshv@axioned.com>
2022-09-06 16:28:01 +0530

    change

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,8 +27,8 @@ function validate() {
 }
 
 function showData() {
-  var cityName = userInput.value ? userInput.value : 'Thane';
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=a4cf8ddd69685b7b0e52be89e98d771f`)
+  // fetch(`https://api.themoviedb.org/3/movie/popular?api_key=5276769952f17e051cb12d48e30645f9`)
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=5276769952f17e051cb12d48e30645f9&query=luck`)
     .then(function (response) {
       try {
         if (response.ok) {
@@ -44,14 +44,14 @@ function showData() {
       }
     })
     .then(function (response) {
-      var output = document.querySelector('.weather-output'),
-        data = `<h2 class="city-heading">${response.name}</h2>`;
-        data += `<h3 class="weather-heading">Weather : <span>${response.weather[0].main}</span></h3>`;
-        data += `<h3 class="weather-heading">Temperature : <span>${Math.round(response.main.temp/10)} &deg;C</span></h3>`;
-        data += `<h3 class="weather-heading">Wind speed : <span>${response.wind.speed} Kmph</span></h3>`;
-
-      output.setAttribute('class', 'weather-output');
-      output.classList.add(response.weather[0].main.toLowerCase());
-      output.innerHTML = data;
+      var data = response.results;
+      data.forEach((movie) => {
+        console.log(movie);
+        console.log('https://image.tmdb.org/t/p/w185//' + movie.poster_path); //img src
+        console.log(movie.title); // movie title
+        console.log(movie.overview); // movie overview
+        console.log(movie.release_date); // movie release_date
+        console.log(movie.vote_average); //movie rating
+      });
     })
 }
